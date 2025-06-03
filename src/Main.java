cfa5db6f843cb376fcd7aff7ed77da7b5f44bdb7
Den2794 <51244326+Den2794@users.noreply.github.com>
2025-06-03 21:06:02 +0500

    ready ti check

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static String name = "";
+    private static int age;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Введите ваш возраст.");
+        setAge(checkingTheNumber());
+        System.out.println("Ваш возвраст " + getAge() + " лет? Круто!");
+        System.out.println("Введите ваше имя.");
+
+        setName(scanner.nextLine());
+        System.out.println("Hello, " + getName() + "!");
+
+    }
+
+    public static int checkingTheNumber(){
+
+        int userInput = 0;
+        boolean isCorrectInput = false;
+        while (!isCorrectInput) {
+            try {
+                userInput = scanner.nextInt();
+                isCorrectInput = true;
+                } catch (InputMismatchException e) {
+                System.out.println("Введите число!");
+            }   finally {
+                scanner.nextLine();
+            }
         }
+
+        return userInput;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String userInput) {
+        name = userInput;
+    }
+
+    public static void setAge(int age) {
+        Main.age = age;
+    }
+
+    public static int getAge() {
+        return age;
     }
 }
